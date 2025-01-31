e340d39dab97c3d540e2dc612436e727fd4e4c91
Mitesh Soni <smiteshhc@gmail.com>
2025-01-31 14:23:04 +0530

    Initial Commit

---
+++ rc.lua
@@ -0,0 +1,16 @@
+--[[
+
+     Awesome WM configuration template
+     github.com/lcpz
+
+--]]
+
+require('lua.required_libs')
+require('lua.error_handling')
+require('lua.autostart')
+require('lua.definitions')
+require('lua.screen')
+require('lua.bindings')
+require('lua.rules')
+require('lua.signals')
+
